91a106616a0d756eb8188a4497fa4348257c5987
levchik2011 <lev.kartsev.00@mail.ru>
2026-06-04 18:26:42 +0500

    Small changes

--- structures_classes/Transaction.cpp
+++ structures_classes/Transaction.cpp
@@ -2,98 +2,95 @@
 #include "picosha2.h"
 #include <cstring>
 #include "bt_secp256k1.h" 
-#include <random>
+#include <array>
 #include <algorithm>
 
-// Метод перевода полей транзакции в единый массив байт для подписи
+
+// Метод перевода полей транзакции в единый массив байт для подписи 
 std::vector<uint8_t> Transaction::serializeForSigning() const {
     std::vector<uint8_t> buffer;
+    buffer.reserve(sender.size() + receiver.size() + 16);
 
     buffer.insert(buffer.end(), sender.begin(), sender.end());
     buffer.insert(buffer.end(), receiver.begin(), receiver.end());
 
-    uint8_t amountBytes[8];
-    std::memcpy(amountBytes, &amount, 8);
-    buffer.insert(buffer.end(), amountBytes, amountBytes + 8);
-
-    uint8_t nonceBytes[8];
-    std::memcpy(nonceBytes, &nonce, 8);
-    buffer.insert(buffer.end(), nonceBytes, nonceBytes + 8);
-
+    for (int i = 0; i < 8; ++i) {
+        buffer.push_back(static_cast<uint8_t>((amount >> (i * 8)) & 0xFF));
+    }
+    for (int i = 0; i < 8; ++i) {
+        buffer.push_back(static_cast<uint8_t>((nonce >> (i * 8)) & 0xFF));
+    }
     // Поля подписи r, s сюда НЕ добввляем так как подпись создается НАД этими байтами!
     return buffer;
 }
+
 std::vector<uint8_t> Transaction::calculateHash() const {
     std::vector<uint8_t> txBytes = serializeForSigning();
-    
-    //хэш с помощью picosha2
     std::vector<uint8_t> hash(32);
     picosha2::hash256(txBytes.begin(), txBytes.end(), hash.begin(), hash.end());
-    
     return hash;
 }
 
-bool Transaction::signTransaction(const vector<uint8_t>& secretKey) {
+bool Transaction::signTransaction(const std::vector<uint8_t>& secretKey) {
     if (secretKey.size() != 32) return false;
 
-    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_SIGN);
-    
-    vector<uint8_t> hash = calculateHash();
-    vector<uint8_t> k(32);
-    random_device rd;
-    bool kValid = false;
+    const secp256k1_context* ctx = secp256k1_context_static;
 
+    if (secp256k1_ec_seckey_verify(ctx, secretKey.data()) != 1) return false;
+
+    std::vector<uint8_t> txHash = calculateHash();
+
+    std::array<uint8_t, 32> k;
+    unsigned int counter = 0;
+    
+    std::vector<uint8_t> k_seed = txHash;
+    k_seed.insert(k_seed.end(), secretKey.begin(), secretKey.end());
+    
+    bool kValid = false;
     while (!kValid) {
-        for (size_t i = 0; i < 8; ++i) {
-            uint32_t chunk = rd();
-            memcpy(&k[i * 4], &chunk, 4);
-        }
+        picosha2::hash256(k_seed.begin(), k_seed.end(), k.begin(), k.end());
         if (secp256k1_ec_seckey_verify(ctx, k.data()) == 1) {
             kValid = true;
+        } else {
+            k_seed.push_back(static_cast<uint8_t>(counter++));
         }
     }
+
     secp256k1_pubkey R_point;
-    if (secp256k1_ec_pubkey_create(ctx, &R_point, k.data()) != 1) {
-        secp256k1_context_destroy(ctx);
-        return false;
-    }
+    if (secp256k1_ec_pubkey_create(ctx, &R_point, k.data()) != 1) return false;
 
-    vector<uint8_t> serialized(33);
-    size_t len = 33;
-    secp256k1_ec_pubkey_serialize(ctx, serialized.data(), &len, &R_point, SECP256K1_EC_COMPRESSED);
-    if (serialized[0] == 0x03) {
+    std::array<uint8_t, 33> serializedR;
+    size_t len = serializedR.size();
+    secp256k1_ec_pubkey_serialize(ctx, serializedR.data(), &len, &R_point, SECP256K1_EC_COMPRESSED);
+
+    std::array<uint8_t, 32> buf_d;
+    std::copy(secretKey.begin(), secretKey.end(), buf_d.begin());
+
+    if (serializedR[0] == 0x03) {
         secp256k1_ec_seckey_negate(ctx, k.data());
-        secp256k1_ec_pubkey_create(ctx, &R_point, k.data());
-        secp256k1_ec_pubkey_serialize(ctx, serialized.data(), &len, &R_point, SECP256K1_EC_COMPRESSED);
+        secp256k1_ec_seckey_negate(ctx, buf_d.data());
     }
 
-    vector<uint8_t> x(32);
-    copy(serialized.begin() + 1, serialized.end(), x.begin());
- 
-    vector<uint8_t> e_buf = x;
+    std::array<uint8_t, 32> x;
+    std::copy(serializedR.begin() + 1, serializedR.end(), x.begin());
+
+    std::vector<uint8_t> e_buf;
+    e_buf.reserve(x.size() + sender.size() + txHash.size());
+    e_buf.insert(e_buf.end(), x.begin(), x.end());
     e_buf.insert(e_buf.end(), sender.begin(), sender.end());
-    e_buf.insert(e_buf.end(), hash.begin(), hash.end());
+    e_buf.insert(e_buf.end(), txHash.begin(), txHash.end());
 
-    vector<uint8_t> e(32);
+    std::array<uint8_t, 32> e;
     picosha2::hash256(e_buf.begin(), e_buf.end(), e.begin(), e.end());
-         
-    vector<uint8_t> buf_d = secretKey;
-    if (secp256k1_ec_privkey_tweak_mul(ctx, buf_d.data(), e.data()) != 1) {
-        secp256k1_context_destroy(ctx);
-        return false;
-    } 
-    vector<uint8_t> s = k; 
-    if (secp256k1_ec_seckey_tweak_add(ctx, s.data(), buf_d.data()) != 1) {
-        secp256k1_context_destroy(ctx);
-        return false;
-    }
 
-    // Сохраняем готовую 64-байтную подпись
+    if (secp256k1_ec_privkey_tweak_mul(ctx, buf_d.data(), e.data()) != 1) return false;
+    if (secp256k1_ec_seckey_tweak_add(ctx, k.data(), buf_d.data()) != 1) return false;
+
     this->sign.clear();
+    this->sign.reserve(64);
     this->sign.insert(this->sign.end(), x.begin(), x.end());
-    this->sign.insert(this->sign.end(), s.begin(), s.end());
+    this->sign.insert(this->sign.end(), k.begin(), k.end());
 
-    secp256k1_context_destroy(ctx);
     return true;
 }
 /*
